cc7e6a2702e199fdf0d43e8810fdf2973e084cab
pleewson <adrianplewait@gmail.com>
2025-10-11 16:14:33 +0200

    zadanie1 -done

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int age;
+
+while (true)
+{
+    Console.Write("Podaj swój wiek ");
+    String input = Console.ReadLine();
+    bool success = int.TryParse(input, out age);
+    if (age > 18)
+    {
+        Console.Write("mozesz robic zakupy ");
+        break;
+    }
+    else if (age >= 14)
+    {
+        Console.Write("mozesz robic zakupy, ale karty sim nie kupisz ");
+        break;
+    }
+    else
+    {
+        Console.Write("nie mozesz robic zakupow " +'\n');
+    }
+}
