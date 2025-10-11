e573fa4ed422b1f119260ba4559fa20f16e2f3a6
pleewson <adrianplewait@gmail.com>
2025-10-11 16:33:00 +0200

    zadanie2 -done

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -1,24 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//ZADANIE_1
+// int age;
+//
+// while (true)
+// {
+//     Console.Write("Podaj swój wiek ");
+//     String input = Console.ReadLine();
+//     bool success = int.TryParse(input, out age);
+//     if (age > 18)
+//     {
+//         Console.Write("mozesz robic zakupy ");
+//         break;
+//     }
+//     else if (age >= 14)
+//     {
+//         Console.Write("mozesz robic zakupy, ale karty sim nie kupisz ");
+//         break;
+//     }
+//     else
+//     {
+//         Console.Write("nie mozesz robic zakupow " +'\n');
+//     }
+// }
 
-int age;
+
+//ZADANIE_2
+
+String poprawneHaslo = "admin123";
 
 while (true)
 {
-    Console.Write("Podaj swój wiek ");
-    String input = Console.ReadLine();
-    bool success = int.TryParse(input, out age);
-    if (age > 18)
+    Console.Write("Podaj haslo ");
+    String twojeHaslo = Console.ReadLine();
+
+    if (twojeHaslo == poprawneHaslo)
     {
-        Console.Write("mozesz robic zakupy ");
+        Console.Write("brawo podales dobre haslo");
         break;
     }
-    else if (age >= 14)
-    {
-        Console.Write("mozesz robic zakupy, ale karty sim nie kupisz ");
-        break;
-    }
-    else
-    {
-        Console.Write("nie mozesz robic zakupow " +'\n');
-    }
+    Console.Write("niepoprawne haslo. sprobuj jeszcze raz");
 }
